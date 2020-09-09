d41bde8e8ca0591e9c4293f1c2c6336fa3d81589
Claudio Souza <jcfigueiredo@gmail.com>
2020-09-08 23:21:30 -0400

    changes to use webcomponents approach

--- appsap/js/rater.js
+++ appsap/js/rater.js
@@ -1,70 +1,80 @@
-export function Rater(containerElement){
+const template  = document.createElement('template');
+template.innerHTML = `
+    <style>
+    .star-rater .star {
+    font-size: 5rem;
+    color: gray;
+    }
+    </style>
+    <div class="star-rater" data-rating="" role="rater">
+        <span class="star" data-value="1">★</span>
+        <span class="star" data-value="2">★</span>
+        <span class="star" data-value="3">★</span>
+        <span class="star" data-value="4">★</span>
+        <span class="star" data-value="5">★</span>
+    </div>
 
-    const createRater = (element) => {
-        const div = document.createElement('div');
-        div.id = element.getAttribute('id');
-        div.classList.add('star-rater');
+`;
 
-        const defaultRating = element.getAttribute('data-default');;
-
-        div.setAttribute('data-rating', defaultRating ? defaultRating : 1 );
-        div.setAttribute('role', 'rater');
-        
-        const allRates = [1, 2, 3, 4, 5];
-        allRates.forEach(idx => {
-            const span = document.createElement('span');
-            span.classList.add('star');
-            span.setAttribute('data-value', idx);
-            span.innerHTML = '&#9733;'
-            div.appendChild(span);
-        });
-
-        return div;
-    
-    };
-
-    const newElement = createRater(containerElement);
-    containerElement.replaceWith(newElement);
-    const ratingElement = document.querySelector('div#' + newElement.id);
-
-    const stars = ratingElement.querySelectorAll('.star');
-
-    const setRating = ev => {
-        ratingElement.setAttribute(
-            'data-rating',
-            ev.currentTarget.getAttribute('data-value')
-        );
-    };
-
-    const ratingHover = ev => {
-        const currentHover = ev.currentTarget.getAttribute('data-value');
-        highlightRating(currentHover);
-
-    };
-
-    const resetRating = ev => {
-        const currentRating = ratingElement.getAttribute('data-rating');
-        highlightRating(currentRating);
+class UserRater extends HTMLElement {
+    starRater = 'div.star-rater';
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open'});    
     }
 
-    const highlightRating = (rating) => {
+    render() {
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        const stars = this.shadowRoot.querySelectorAll('.star');
+        
+        const divRater = this.shadowRoot.querySelector(this.starRater);
+
+        divRater.id = this.getAttribute('id');
+        divRater.setAttribute('data-rating', this.getAttribute('data-default'));
+
+        this.resetRating();
+
+    }
+
+    highlightRating(rating) {
+        const stars = this.shadowRoot.querySelectorAll('.star');
         stars.forEach(star => {
-            
             star.style.color = 
                 rating >= star.getAttribute('data-value') ? 'yellow' : 'gray';
         });
     };
 
-    resetRating();
+    setRating(ev) {
+        this.shadowRoot.querySelector(this.starRater).setAttribute(
+            'data-rating',
+            ev.currentTarget.getAttribute('data-value')
+        );
+    };
 
+    ratingHover(ev) {
+        const currentHover = ev.currentTarget.getAttribute('data-value');
+        this.highlightRating(currentHover);
 
+    };
 
+    resetRating() {
+        const currentRating = this.shadowRoot.querySelector(this.starRater).getAttribute('data-rating');
+        this.highlightRating(currentRating);
+    };
 
-    stars.forEach(star => {
-        star.addEventListener('click', setRating);
-        star.addEventListener('mouseover', ratingHover);
-    });
+    connectedCallback() {
+        this.render();
 
-    ratingElement.addEventListener('mouseout', resetRating);
+        this.shadowRoot.addEventListener('mouseout', () => this.resetRating());
 
+        const stars = this.shadowRoot.querySelectorAll('.star');
+
+        stars.forEach(star => {
+            star.addEventListener('click', (ev) => this.setRating(ev));
+            star.addEventListener('mouseover', (ev) => this.ratingHover(ev));
+        });
+    }
 }
+
+window.customElements.define('user-rater', UserRater);
